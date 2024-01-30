af45a38b447a42341fe0e7c76f6fe88e5327f152
Razmus R <Razmus.Rosen@hotmail.com>
2024-01-30 16:12:30 +0100

    Testing new branch

--- PigDiceGame.py
+++ PigDiceGame.py
@@ -1,7 +1,11 @@
+import os
 import random
 
 
 class Dice:
+    def __init__(self):
+        self.diceValue = 1
+
     def getDiceValue(self):
         self.diceValue = random.randint(1, 6)
         return self.diceValue
@@ -17,47 +21,63 @@ class Player:
 
     def tossDice(self, dice):
         value = dice.getDiceValue()
-        print("You got a " + str(value))
+        print(self.getPlayerName() + " got a " + str(value))
         if value != 1:
             self.setPoints(value)
             return self.points
         else:
-            return 1
+            value = 0
+            self.setPoints(value)
+            return self.points
 
     def setPoints(self, value):
-        self.points += value
+        if value != 0:
+            self.points += value
+        else:
+            self.points = value
 
     def getPoints(self):
         return self.points
+
+
+class HighScore:
+    def __init__(self):
+        self.file = "HighScores.txt"
+        self.createFile(self.file)
+
+    def createFile(self, file):
+        if os.path.exists(file):
+            pass
+        else:
+            with open(file, "w") as file:
+                file.write("High scores")
 
 
 def main():
     player1 = Player()
     player2 = Player()
     dice = Dice()
+    HighScore()
 
     playing = True
     while (playing):
         player1Points = player1.tossDice(dice)
-        player2Points = player2.tossDice(dice)
-
-        print("Player: " + player1.getPlayerName() + " got " + str(player1Points) + " points")
-        print("Player: " + player2.getPlayerName() + " got " + str(player2Points) + " points")
-
-        if (player1Points == 1):
+        print("Player: " + player1.getPlayerName() + " got " + str(player1Points) + " point(s)")
+        if (player1Points == 0):
             print(player1.getPlayerName() + " lost")
-            playing = False
-        elif (player2Points == 1):
+            break
+        
+        player2Points = player2.tossDice(dice)
+        print("Player: " + player2.getPlayerName() + " got " + str(player2Points) + " point(s)")
+        if (player2Points == 0):
             print(player2.getPlayerName() + " lost")
-            playing = False
-
+            break
+            
         if playing is True:
             choice = input("Do you want to continue? ")
             if choice == "no":
                 playing = False
-        else:
-            continue
-
+printafasddadsada
 
 if __name__ == '__main__':
     main()
