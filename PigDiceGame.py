35017b25f0a9e037e131f2f3c7a6e2d20bb4b876
Razmus R <Razmus.Rosen@hotmail.com>
2024-02-10 23:14:21 +0100

    Fixed a bunch of stuff

--- PigDiceGame.py
+++ PigDiceGame.py
@@ -6,9 +6,8 @@ class Dice:
     def __init__(self):
         self.diceValue = 1
 
-    def getDiceValue(self):
-        self.diceValue = random.randint(1, 6)
-        return self.diceValue
+    def getRandomDiceValue(self):
+        return random.randint(1, 6)
 
     def getValue(self):
         return self.diceValue
@@ -23,21 +22,12 @@ class Player:
         return self.playerName
 
     def tossDice(self, dice):
-        value = dice.getDiceValue()
+        value = dice.getRandomDiceValue()
         print(self.getPlayerName() + " got a " + str(value))
-        if value != 1:
-            self.setPoints(value)
-            return self.points
-        else:
-            value = 0
-            self.setPoints(value)
-            return self.points
+        return value
 
     def setPoints(self, value):
-        if value != 0:
-            self.points += value
-        else:
-            self.points = value
+        self.points += value
 
     def getPoints(self):
         return self.points
@@ -84,6 +74,11 @@ class HighScore:
             return player2.getPlayerName() + " won!"
         else:
             print()
+            
+
+class Game:
+    def __init__(self) -> None:
+        pass
 
 
 def main():
@@ -94,39 +89,46 @@ def main():
 
     player1Plays = True
     player2Plays = True
-    
-    while (player1Plays):
-        player1Points = player1.tossDice(dice)
-        print("Player: " + player1.getPlayerName() + " got " + str(player1Points) + " point(s)")
-        if (player1Points != 0):
-            choice = input("Do you want to stay or toss? ")
-            if (choice == "toss"):
-                continue
-            else:
-                print("Okay you stayed with " + str(player1Points) + " point(s)")
+    player1Point = player1.getPoints()
+    player2Point = player2.getPoints()
+
+    while (player1Plays or player2Plays and player1Point < 100 or player2Point < 100):
+
+        if (player1Plays is True):
+            player1Points = player1.tossDice(dice)
+            if (player1Points != 1):
+                player1Point += player1Points
+                print(player1.getPlayerName() + " have " + str(player1Point) + " point(s)")
+                choice = input("Do you want to stay or toss? ")
+                if (choice == "toss"):
+                    continue
+                else:
+                    print("Okay " + player1.getPlayerName() + " stayed with " + str(player1Point) + " point(s)\n")
+                    player1.setPoints(player1Point)
+                    player1Plays = False
+                    player2Plays = True
+            elif (player1Points == 1):
+                print(player1.getPlayerName() + " have " + str(player1.getPoints()) + " point(s)\n")
                 player1Plays = False
-        else:
-            player1Plays = False
-            player2Points = player2.tossDice(dice)
-            print("Player: " + player2.getPlayerName() + " got " + str(player2Points) + " point(s)")
-            if (player2Points == 0):
-                print("It's a tie")
-                player2Plays = False
+                player2Plays = True
 
-    while (player2Plays):
-        player2Points = dice.getValue
-        if (player1Points == 0):
-            print(player2.getPlayerName() + " you won!")
-            player2Plays = False
-        elif (player2Points != 0):
+        elif (player2Plays is True and player1Plays is False):
             player2Points = player2.tossDice(dice)
-            print("Player: " + player2.getPlayerName() + " got " + str(player2Points) + " point(s)")
-
-            choice = input("Do you want to stay or toss? ")
-            if (choice == "toss"):
-                continue
-            else:
+            if (player2Points != 1):
+                player2Point += player2Points
+                print(player2.getPlayerName() + " have " + str(player2Point) + " point(s)")
+                choice = input("Do you want to stay or toss? ")
+                if (choice == "toss"):
+                    continue
+                else:
+                    player2.setPoints(player2Point)
+                    print("Player: " + player2.getPlayerName() + " got " + str(player2Point) + " point(s)\n")
+                    player2Plays = False
+                    player1Plays = True
+            elif (player2Points == 1):
+                print(player2.getPlayerName() + " have " + str(player2.getPoints()) + " point(s)\n")
                 player2Plays = False
+                player1Plays = True
 
     result = HighScoreManager.whoWon(player1, player2)
     print(result)
